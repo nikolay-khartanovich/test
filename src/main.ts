eee873a78dbd7bac759678b2fc42c67b59a88890
nikolay-khartanovich <nikolay.khartanovich@efficiently.com>
2025-04-24 10:28:39 +0300

    5

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { tryLoadAndStartRecorder } from '@alwaysmeticulous/recorder-loader';
+(async (): Promise<void> => {
+	try {
+		// Start the Meticulous recorder before initializing the app
+		await tryLoadAndStartRecorder({
+			recordingToken: 'Jov6UfIpDIRX6qCvkjed2hrfBO7Q7fBMKtAZ1HHd',
+			projectId: 'Test-Meticulos',
+		});
+	} catch (err) {
+		console.error(`Meticulous failed to initialise: ${err}`);
+	}
 
-bootstrapApplication(AppComponent, appConfig)
+  bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+})();
