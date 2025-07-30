381e5081844c24b09f66f6dfdb5ed009683c750e
nikolay-khartanovich <nikolay.khartanovich@efficiently.com>
2025-07-30 15:57:07 +0300

    вы

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import { tryLoadAndStartRecorder } from '@alwaysmeticulous/recorder-loader';
 			projectId: 'Test-Meticulos',
 		});
 	} catch (err) {
-		console.error(`Meticulous failed to initialise: ${err223}`);
+		console.error(`Meticulous failed to initialise: ${errмс}`);
 	}
 
   bootstrapApplication(AppComponent, appConfig)
