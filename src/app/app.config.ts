5d8bc2ffce1f3b315e9c903a565585aca4f45bd6
nikolay-khartanovich <nikolay.khartanovich@efficiently.com>
2025-07-31 13:34:14 +0300

    f

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angulxar/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing:xx true }), provideRouter(routes)]
 };
